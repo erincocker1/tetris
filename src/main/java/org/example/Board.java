94fd095b50af2d6eace5121fdc89288ee3182dec
erincocker1 <erin.cocker@ten10.com>
2024-11-01 16:09:56 +0000

    Line clears!

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -20,5 +20,24 @@ public class Board {
     }
 
     public void checkForFullLine() {
+        int fullLineCount = 0;
+        boolean currentLineFull;
+        for (int j=19; j>=0; j--) {
+            currentLineFull = true;
+            for (int i=0; i<10; i++) {
+                if (grid[i][j] == TileType.BLANK) {
+                    currentLineFull = false;
+                    break;
+                }
+            }
+
+            if (currentLineFull) {
+                fullLineCount += 1;
+            } else if (fullLineCount > 0) { //move current row down {fullLineCount} rows
+                for (int i=0; i<10; i++) {
+                    grid[i][j+fullLineCount] = grid[i][j];
+                }
+            }
+        }
     }
 }
